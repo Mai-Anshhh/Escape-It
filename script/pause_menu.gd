70a7642c498b6c0bdb76dfa2f278679c8976a875
Mai-Anshhh <rajs002ssp@gmail.com>
2026-07-11 11:03:56 +0530

    added pause menu

---
+++ script/pause_menu.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+@onready var start: AudioStreamPlayer = $start
+@onready var quit: AudioStreamPlayer = $quit
+
+func _ready() -> void:
+	visible = false
+	get_tree().paused = false
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		if get_tree().paused:
+			visible = false
+			get_tree().paused = false
+		else:
+			visible = true
+			get_tree().paused = true
+
+func _on_resume_pressed() -> void:
+	start.play()
+	visible = false
+	get_tree().paused = false
+
+
+func _on_main_menu_pressed() -> void:
+	quit.play()
+	visible = false
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
